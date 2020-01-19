1fa6ae8fb0f90969e747ccca8f1a56ec18bdb197
Adrian Reed Smith <adrian@smithdev.io>
2020-01-18 17:30:56 -0800

    Fixing and updating various things. 2020 refresh

--- src/app/reading-list/readlist-json.ts
+++ src/app/reading-list/readlist-json.ts
@@ -1,81 +1,5 @@
 export let readlistJson: {}[] = [
   {
-    title: 'I Shall Seal the Heavens',
-    author: 'Er Gen',
-    genre: ['fiction', 'Wuxia', 'fantasy'],
-    details: 'A compelling and unique introduction into the world of Chinese martial arts fiction. The main character Meng Hao sets out' +
-    ' on a journey that is reminiscent of ancient Greek epics but has a unique flavor due to the different cultural traditions of China' +
-    ' as compared to Western fiction.\n' +
-    'For me, this novel was the epitome of a page turner, and made me laugh and cry more than many other books that I would consider' +
-    ' to be more technically impressive. The story has it’s share of Wuxia tropes, but it breaks past all of those with the wit and' +
-    ' charm of its characters. If you have the time to spend on the over 1600 chapters of adventure this story provides, the time' +
-    ' investment will reveal a whole new world of fantasy storytelling to you.',
-    tagline: '"Snow can only live in the winter. When it nears a fire, it dies. That is its life. It may yearn for summer, but… ' +
-    'it can only desire it. In my hand, the snow becomes water, because this is not its world…"\n ― Meng Hao',
-    img: '/assets/images/readimages/issth.jpg',
-    url: 'http://www.wuxiaworld.com/issth-index/'
-  },
-  {
-    title: 'Harry Potter and the Methods of Rationality',
-    author: 'Eliezer Yudkowsky',
-    genre: ['fiction', 'fantasy'],
-    details: 'For all of its flaws, few books have resonated with me more than this one. The whole story exudes a particular charm that' +
-    ' comes from Eliezer\'s skill at mixing the tragedy and comedy of the story that gives you breaks when you need them but never lets' +
-    ' the reader completely relax their guard. Harry Potter was a big part of my childhood and revisiting the universe having seen more' +
-    ' of the world I found this book’s take on the world to be more than satisfying.\n' +
-    'If you want to see what it looks like when an fun magic coming of age story meets an engineer with a penchant for apocalypse,' +
-    ' then read on (also there’s an audio version!).',
-    tagline: '“And someday when the descendants of humanity have spread from star to star they won’t tell the children about the history' +
-    ' of Ancient Earth until they’re old enough to bear it and when they learn they’ll weep to hear that such' +
-    ' a thing as Death had ever once existed”\n ― Harry Potter',
-    img: '/assets/images/readimages/hpmor.jpg',
-    url: 'http://www.hpmor.com/'
-  },
-  {
-    title: 'Rationality: From AI to Zombies',
-    author: 'Eliezer Yudkowsky',
-    genre: ['nonfiction', 'philosophy'],
-    details: 'How do we talk about what it means to know something? How can we make decisions given the uncertainty of the world we live' +
-    ' in? These questions fall within the wonderful realm of epistemics. This book which compiles Eliezer Yudkowsky’s writings is a' +
-    ' great introduction to thinking critically about the nature of knowledge, as well as containing some interesting arguments about' +
-    ' consciousness and morality. With distance I see more flaws in the book than when I first read it, but that doesn’t change the' +
-    ' fact that I still see the book as the most accessible way to learn how to be a better critical thinking. Additional, the book' +
-    ' draws heavily on several sources and so reading those is sure to provide hours of extra contemplation and will help to expand' +
-    ' upon the book’s somewhat brief treatment of many subjects.',
-    tagline: '"If there is no black and white, there is yet lighter and darker, and not all grays are the same."\n ― Eliezer Yudkowsky',
-    img: '/assets/images/readimages/rationality.jpg',
-    url: 'https://intelligence.org/rationality-ai-zombies/'
-  },
-  {
-    title: 'Surely You\'re Joking, Mr. Feynman!',
-    author: 'Richard P. Feynman',
-    genre: ['nonfiction', 'autobiography'],
-    details: 'Richard Feynman has gotten a bad rap. I don’t think it’s unfair to say that he has been a large influence in forming' +
-    ' what we see as the stereotypical quirky physicist. However, I will stand by my recommendation of this great book about the life' +
-    ' of a guy who had that rare skill of making the complex simple. Feynman’s skill at communication is unparalleled, and he led a' +
-    ' life that was full of twists and turns - from his work on the Manhattan project to the death of his wife. Inspiring characters' +
-    ' help us to push ourselves further, and I think Feynman did was just that.',
-    tagline: '"Of course, you only live one life, and you make all your mistakes, ' +
-    'and learn what not to do, and that’s the end of you."\n ― Richard Feynman',
-    img: '/assets/images/readimages/feynman.jpg',
-    url: 'https://www.amazon.com/Surely-Feynman-Adventures-Curious-Character/dp/0393316041'
-  },
-  {
-    title: 'Seeing like a State: How Certain Schemes to Improve the Human Condition Have Failed',
-    author: 'James C. Scott',
-    genre: ['nonfiction', 'political philosophy', 'history'],
-    details: 'For someone like me with naturally utopian tendencies, this book was hard to read. I spent much of my life thinking' +
-    ' about how we could use technology and science to bring order and improve the world. However, this book forced me to confront' +
-    ' the reality that the naturally evolved mechanisms of society are more delicate than they might seem, and that to fix them may' +
-    ' cause vast amounts of unexpected harm. I recommend this book heavily to anyone with an interest in the social and moral future' +
-    ' of humanity, and the dangers that lay on the sides of the road.',
-    tagline: '"We are now so prone to confuse great building projects with great social achievements. We will have to admit that it is' +
-    ' beyond the scope of anyone’s imagination to create a community. We must learn to cherish the communities we have,' +
-    ' they are hard to come by."\n ― James C. Scott',
-    img: '/assets/images/readimages/seelikeastate.jpg',
-    url: 'https://www.amazon.com/Seeing-like-State-Certain-Condition/dp/0300078153'
-  },
-  {
     title: 'The Wind-Up Bird Chronicle',
     author: 'Haruki Murakami',
     genre: ['fiction', 'historical fiction'],
@@ -84,7 +8,7 @@ export let readlistJson: {}[] = [
     ' stayed in my head for years. I recommend it to anyone who likes the surreal and wants a mystery that will leave them feeling' +
     ' both satisfied and confused.',
     tagline: '"We were young, and we had no need for prophecies. Just living was itself an act of prophecy."\n ― Haruki Murakami',
-    img: '/assets/images/readimages/windupbird.jpg',
+    img: '/assets/images/readimages/windupbird.webp',
     url: 'https://www.amazon.com/Wind-Up-Bird-Chronicle-Vintage-International-ebook/dp/B003XT605Y'
   },
   {
@@ -101,16 +25,157 @@ export let readlistJson: {}[] = [
     url: 'https://www.amazon.com/exec/obidos/ASIN/0552131067/'
   },
   {
+    title: 'Practical Guide to Evil',
+    author: 'ErraticErrata',
+    genre: ['fiction', 'fantasy'],
+    details: 'Maybe the best piece of fiction I\'ve read in the 2010s (though we\'ll have to see the series end to be sure.)' +
+    'The novel has a huge cast of characters, every one of which is fascinating in their own way. Despite that, the book never feels' +
+    'unfocused like some ensemble cast novels. The setup of the world is amazing, and plays with some very interesting ideas. The' +
+    'combination of this dynamic and charismatic cast alongside the author\'s ability to spin a world that you\'ll be hard pressed' +
+    'to stop thinking about will keep you reading compulsively deep into the night. Also, the author\'s ability to continually make striking' +
+    'moments that hit hard is super impressive. This is a must read for any fan of fiction.',
+    tagline: '"Even victories ordained by the Heavens can be broken by the will of men.\n' +
+    'But blame doesn’t matter. Never has, never will. Villains must attend to reality or be swallowed by it."\n ― The Black Knight',
+    img: '/assets/images/readimages/pgte.webp',
+    url: 'https://practicalguidetoevil.wordpress.com/'
+  },
+  {
+    title: 'Overgeared',
+    author: 'Park Saenal',
+    genre: ['fiction', 'virtual-reality', 'fantasy'],
+    details: 'A great popcorn fiction read, and one of the better Korean novels. The main character has an entertaining arc, ' +
+    'a rarity in webnovels. The world is compelling, and the author does a good job following their own rules. Also, the novel has a fair bit' +
+    'of lighthearted comedy, which helps make the book a nice way to wind down after a day\'s workI definitely recommend' +
+    'the book to any fantasy books looking for a time sink.',
+    tagline: '',
+    img: '/assets/images/readimages/og.webp',
+    url: 'https://www.wuxiaworld.com/novel/overgeared'
+  },
+  {
+    title: 'I Shall Seal the Heavens',
+    author: 'Er Gen',
+    genre: ['fiction', 'Wuxia', 'fantasy'],
+    details: 'A compelling and unique introduction into the world of Chinese martial arts fiction. The main character Meng Hao sets out' +
+    ' on a journey that is reminiscent of ancient Greek epics but has a unique flavor due to the different cultural traditions of China' +
+    ' as compared to Western fiction.\n' +
+    'For me, this novel was the epitome of a page turner, and made me laugh and cry more than many other books that I would consider' +
+    ' to be more technically impressive. The story has it’s share of Wuxia tropes, but it breaks past all of those with the wit and' +
+    ' charm of its characters. If you have the time to spend on the over 1600 chapters of adventure this story provides, the time' +
+    ' investment will reveal a whole new world of fantasy storytelling to you.',
+    tagline: '"Snow can only live in the winter. When it nears a fire, it dies. That is its life. It may yearn for summer, but… ' +
+    'it can only desire it. In my hand, the snow becomes water, because this is not its world…"\n ― Meng Hao',
+    img: '/assets/images/readimages/issth.webp',
+    url: 'https://www.wuxiaworld.com/novel/i-shall-seal-the-heavens'
+  },
+  {
+    title: 'Harry Potter and the Methods of Rationality',
+    author: 'Eliezer Yudkowsky',
+    genre: ['fiction', 'fantasy'],
+    details: 'For all of its flaws, few books have resonated with me more than this one. The whole story exudes a particular charm that' +
+    ' comes from Eliezer\'s skill at mixing the tragedy and comedy of the story that gives you breaks when you need them but never lets' +
+    ' the reader completely relax their guard. Harry Potter was a big part of my childhood and revisiting the universe having seen more' +
+    ' of the world I found this book’s take on the world to be more than satisfying.\n' +
+    'If you want to see what it looks like when an fun magic coming of age story meets an engineer with a penchant for apocalypse,' +
+    ' then read on (also there’s an audio version!).',
+    tagline: '“And someday when the descendants of humanity have spread from star to star they won’t tell the children about the history' +
+    ' of Ancient Earth until they’re old enough to bear it and when they learn they’ll weep to hear that such' +
+    ' a thing as Death had ever once existed”\n ― Harry Potter',
+    img: '/assets/images/readimages/hpmor.webp',
+    url: 'http://www.hpmor.com/'
+  },
+  {
+    title: 'Seeing like a State: How Certain Schemes to Improve the Human Condition Have Failed',
+    author: 'James C. Scott',
+    genre: ['nonfiction', 'political philosophy', 'history'],
+    details: 'For someone like me with naturally utopian tendencies, this book was hard to read. I spent much of my life thinking' +
+    ' about how we could use technology and science to bring order and improve the world. However, this book forced me to confront' +
+    ' the reality that the naturally evolved mechanisms of society are more delicate than they might seem, and that to fix them may' +
+    ' cause vast amounts of unexpected harm. I recommend this book heavily to anyone with an interest in the social and moral future' +
+    ' of humanity, and the dangers that lay on the sides of the road.',
+    tagline: '"We are now so prone to confuse great building projects with great social achievements. We will have to admit that it is' +
+    ' beyond the scope of anyone’s imagination to create a community. We must learn to cherish the communities we have,' +
+    ' they are hard to come by."\n ― James C. Scott',
+    img: '/assets/images/readimages/seelikeastate.webp',
+    url: 'https://www.amazon.com/Seeing-like-State-Certain-Condition/dp/0300078153'
+  },
+  {
+    title: 'Open Borders: The Science and Ethics of Immigration',
+    author: 'Bryan Caplan (Author), Zach Weinersmith (Illustrator)',
+    genre: ['nonfiction', 'political philosophy', 'comic'],
+    details: 'An excellent introduction to the topic of border regulations for both the policy fan who keeps up to date on the latest' +
+    'debates and the casual voter who just wants to better understand immigration policy. Compelling arguments and a sense of humor' +
+    'help the book communicate effectively without feeling preachy. Highly recommend this read to basically everyone.',
+    tagline: '',
+    img: '/assets/images/readimages/openborders.webp',
+    url: 'https://www.amazon.com/Open-Borders-Science-Ethics-Immigration/dp/1250316960'
+  },
+  {
+    title: 'How Asia Works',
+    author: 'Joe Studwell',
+    genre: ['nonfiction', 'political philosophy', 'asia'],
+    details: 'Honorable mention for the best political science book I\'ve read in the past 10 years. Even if you come out the other side' +
+    'disagreeing with the author\'s judgement about how best to run a developing economy, it\'s still 100% worth reading for the ' +
+    'history of land reform and industrial policy in Asia. The later bits on financialization are less compelling, and by far' +
+    'the weakest part of the book. An absolute must read for fans of history or political science.',
+    tagline: '"Korea and Taiwan went from being the world’s 33rd and 28th leading exporters in 1965 to being the 13th and 10th respectively' +
+    'twenty years later. At that point, both economies boasted greater manufacturing exports than the whole of Latin America."\n' +
+    ' ― Joe Studwell',
+    img: '/assets/images/readimages/howasiaworks.webp',
+    url: 'https://www.amazon.com/How-Asia-Works-Joe-Studwell/dp/0802121322'
+  },
+  {
+    title: 'Rationality: From AI to Zombies',
+    author: 'Eliezer Yudkowsky',
+    genre: ['nonfiction', 'philosophy'],
+    details: 'How do we talk about what it means to know something? How can we make decisions given the uncertainty of the world we live' +
+    ' in? These questions fall within the wonderful realm of epistemics. This book which compiles Eliezer Yudkowsky’s writings is a' +
+    ' great introduction to thinking critically about the nature of knowledge, as well as containing some interesting arguments about' +
+    ' consciousness and morality. With distance I see more flaws in the book than when I first read it, but that doesn’t change the' +
+    ' fact that I still see the book as the most accessible way to learn how to be a better critical thinking. Additional, the book' +
+    ' draws heavily on several sources and so reading those is sure to provide hours of extra contemplation and will help to expand' +
+    ' upon the book’s somewhat brief treatment of many subjects.',
+    tagline: '"If there is no black and white, there is yet lighter and darker, and not all grays are the same."\n ― Eliezer Yudkowsky',
+    img: '/assets/images/readimages/rationality.webp',
+    url: 'https://intelligence.org/rationality-ai-zombies/'
+  },
+  {
+    title: 'Surely You\'re Joking, Mr. Feynman!',
+    author: 'Richard P. Feynman',
+    genre: ['nonfiction', 'autobiography'],
+    details: 'Richard Feynman has gotten a bad rap. I don’t think it’s unfair to say that he has been a large influence in forming' +
+    ' what we see as the stereotypical quirky physicist. However, I will stand by my recommendation of this great book about the life' +
+    ' of a guy who had that rare skill of making the complex simple. Feynman’s skill at communication is unparalleled, and he led a' +
+    ' life that was full of twists and turns - from his work on the Manhattan project to the death of his wife. Inspiring characters' +
+    ' help us to push ourselves further, and I think Feynman did was just that.',
+    tagline: '"Of course, you only live one life, and you make all your mistakes, ' +
+    'and learn what not to do, and that’s the end of you."\n ― Richard Feynman',
+    img: '/assets/images/readimages/feynman.webp',
+    url: 'https://www.amazon.com/Surely-Feynman-Adventures-Curious-Character/dp/0393316041'
+  },
+  {
     title: 'Slate Star Codex',
     author: 'Scott Alexander',
     genre: ['blog', 'science', 'philosophy', 'rationality'],
     details: 'Slate Star Codex is a collection of some of the most interesting and insightful passages on the internet. Every post is' +
     ' full of careful reasoning and consistent use of evidence. This blog has made me think more than many books have, and I recommend' +
     ' that almost everybody give it a shot.',
-    tagline: 'All good is hard. All evil is easy. Dying, losing, cheating, and mediocrity is easy. ' +
+    tagline: '"All good is hard. All evil is easy. Dying, losing, cheating, and mediocrity is easy. ' +
     'Stay away from easy."\n ― Scott Alexander',
     img: '/assets/images/readimages/codex_bg.webp',
     url: 'http://slatestarcodex.com/'
+  },
+  {
+    title: 'Shtetl-Optimized',
+    author: 'Scott Aaronson',
+    genre: ['blog', 'science', 'computing'],
+    details: 'Shtetl-Optimized got me interested in quantum computing, and probably has the best intro to the topic for CS/Math types' +
+    'anywhere I\'ve read. It\'s full of insight for the reader who\'s only heard of this quantum stuff in school and found it completely' +
+    'indecipherable, and to the experienced researcher who wants to keep up with the latest advancements in the field.',
+    tagline: '"If you take just one piece of information from this blog:\n' +
+    'Quantum computers would not solve hard search problems\n' +
+    'instantaneously by simply trying all the possible solutions at once."\n ― Scott Aaronson',
+    img: '/assets/images/readimages/shtetl.webp',
+    url: 'https://www.scottaaronson.com/blog/'
   },
   // {
   //   title: '',
